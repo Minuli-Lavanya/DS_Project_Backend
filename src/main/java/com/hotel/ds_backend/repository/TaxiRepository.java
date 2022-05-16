91b99aae4b5eddd7c82198256267af09a1edd14e
Minuli Lavanya <minuli.bambarandage@axiatadigitallabs.com>
2022-05-16 10:32:01 +0530

    add taxi service repository

---
+++ src/main/java/com/hotel/ds_backend/repository/TaxiRepository.java
@@ -0,0 +1,9 @@
+package com.hotel.ds_backend.repository;
+
+import com.hotel.ds_backend.model.Taxi;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaxiRepository extends JpaRepository<Taxi, Long> {
+}
