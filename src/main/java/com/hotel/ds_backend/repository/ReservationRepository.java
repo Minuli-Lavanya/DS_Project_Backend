2ab1fa9b82f62cbcc4881f589548fcec3d040d06
Minuli Lavanya <minuli.bambarandage@axiatadigitallabs.com>
2022-05-16 10:27:30 +0530

    add reservation Repository

---
+++ src/main/java/com/hotel/ds_backend/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.hotel.ds_backend.repository;
+
+import com.hotel.ds_backend.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
